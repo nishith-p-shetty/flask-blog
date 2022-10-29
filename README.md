7110cf874dc188834d1b5de8ee744fa62600f759
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-10-29 16:37:51 +0530

    1st Commit🎉

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# flask-blog
+# BLOG
+#### Video Demo:  <URL HERE>
+#### Description:
+TODO
+
+
 * A blogging website made with flask backend
+* It uses Bootsprap CSS, Bootstrap Templates
 * written in python 3.5.9
 
 # Installation
 * run pip install requirments.txt
 * fill your details in config.json file
 * import clean_blog.sql to your mysql server
-* allow  "less secure apps" in your google account which you are trying to login
+* or sqlite3 clean_blog.db
+* .read clean_blog.sql
 * then run python main.py
--- clean_blog.sql
+++ clean_blog.sql
@@ -1,93 +1,36 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 09, 2021 at 07:01 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- import to SQLite by running: sqlite3.exe db.sqlite3 -init sqlite.sql
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+PRAGMA journal_mode = MEMORY;
+PRAGMA synchronous = OFF;
+PRAGMA foreign_keys = OFF;
+PRAGMA ignore_check_constraints = OFF;
+PRAGMA auto_vacuum = NONE;
+PRAGMA secure_delete = OFF;
+BEGIN TRANSACTION;
 
+CREATE TABLE contacts (
+sno INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+phone_num TEXT NOT NULL,
+msg TEXT NOT NULL,
+date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+email TEXT NOT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE posts (
+sno INTEGER PRIMARY KEY AUTOINCREMENT,
+title TEXT NOT NULL,
+subtitle TEXT NOT NULL,
+slug TEXT NOT NULL,
+content TEXT NOT NULL,
+author TEXT NOT NULL,
+img_file TEXT NOT NULL,
+date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
---
--- Database: `clean_blog`
---
-CREATE DATABASE IF NOT EXISTS `clean_blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `clean_blog`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `contacts`
---
-
-CREATE TABLE `contacts` (
-  `sno` int(50) NOT NULL,
-  `name` text NOT NULL,
-  `phone_num` varchar(50) NOT NULL,
-  `msg` text NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-CREATE TABLE `posts` (
-  `sno` int(50) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `subtitle` varchar(50) NOT NULL,
-  `slug` varchar(20) NOT NULL,
-  `content` varchar(1000) NOT NULL,
-  `author` varchar(20) NOT NULL,
-  `img_file` varchar(50) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `contacts`
---
-ALTER TABLE `contacts`
-  ADD PRIMARY KEY (`sno`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`sno`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `contacts`
---
-ALTER TABLE `contacts`
-  MODIFY `sno` int(50) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `sno` int(50) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+PRAGMA ignore_check_constraints = ON;
+PRAGMA foreign_keys = ON;
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;
